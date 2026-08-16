4631e2efad90ab0b70581fd0bdc67ce758974a2a
RaeHyunTheHobbang <kangraehyun512@gmail.com>
2026-08-17 00:42:21 +0900

    [Passed] 마을 구분하기 (56ms, 17MB)

---
+++ trail4/마을 구분하기/seperate-village.py
@@ -0,0 +1,49 @@
+import sys
+from collections import deque
+
+n=int(sys.stdin.readline().strip())
+graph=[]
+dtmp=[-1,1,0,0]
+visited=[[0 for _ in range(n)] for _ in range(n)]
+
+
+def dfs(cur_r,cur_c):
+    global graph,visited,dtmp,n
+
+    stack=deque()
+    stack.append((cur_r,cur_c))
+    result=0
+
+    while stack:
+        cur_r,cur_c=stack.pop()
+        for tr,tc in zip(dtmp,dtmp[::-1]):
+            next_r,next_c=cur_r+tr,cur_c+tc
+            if 0<=next_r<n and 0<=next_c<n:
+                if visited[next_r][next_c]==0 and graph[next_r][next_c]==1:
+                    visited[next_r][next_c]=1
+                    result+=1
+                    stack.append((next_r,next_c))
+
+    return result
+    
+
+
+answer=[]
+for _ in range(n):
+    line=list(map(int,sys.stdin.readline().split()))
+    graph.append(line)
+
+
+for r in range(n):
+    for c in range(n):
+        if visited[r][c]==0 and graph[r][c]==1:
+            answer.append(dfs(r,c))
+
+
+print(len(answer))
+
+for cur in sorted(answer):
+    if cur==0:
+        print(1)
+    else:
+        print(cur)
